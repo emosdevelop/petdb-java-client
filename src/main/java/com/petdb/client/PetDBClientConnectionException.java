1de7b2857493365fb520ec5bc556ccf99e28077f
EMOS <emosdevelop@gmail.com>
2020-11-23 16:08:26 +0100

    Added custom exception PetDBClientConnectionException

---
+++ src/main/java/com/petdb/client/PetDBClientConnectionException.java
@@ -0,0 +1,7 @@
+package com.petdb.client;
+
+public class PetDBClientConnectionException extends RuntimeException {
+    public PetDBClientConnectionException(String message) {
+        super(message);
+    }
+}
